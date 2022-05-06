f701654517b45b8932720912fd5093e93c5ac738
brook <brook@DESKTOP-QI0KJKS>
2022-05-05 20:19:40 -0400

    Let go...

--- src/test/java/git2/Demo2.java
+++ src/test/java/git2/Demo2.java
@@ -5,14 +5,8 @@ public class Demo2 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Git demo 2...");
-		System.out.println("Git demo 5...");
+		System.out.println("Git demo 20...");
 		
-		System.out.println("Git demo 9...");
-		
-		System.out.println("Git demo 10...");
-		
-		System.out.println("Git demo 11...");
 		
 	}
 	
